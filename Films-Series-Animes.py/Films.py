fe11cc51ff322faf31c7ccb013120225565bdc2d
Bora Saglam <posarobit@gmail.com>
2025-06-04 17:24:57 +0300

    Films.py değişiklik yaptım

--- Films-Series-Animes.py/Films.py
+++ Films-Series-Animes.py/Films.py
@@ -2,6 +2,6 @@ mesaj = input("Films Genres")
 
 mesaj = input("1. Science-Fiction films")
 
-mesaj = input("Uzaydan Gelen Firtina", "Lucy", "Ready Player One", "Gelecege Donus", "Adam Project", "Ask ve Canavarlar", "Terminator", "Mad Max", "Morbius")
+film1 = ("Uzaydan Gelen Firtina")
 
 
