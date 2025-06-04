d2a7920580c69c8121cf3edf13c0bb0bfbc63a2d
Bora Saglam <posarobit@gmail.com>
2025-06-04 16:57:15 +0300

    HesapMakinasi.py değişiklik yaptım

--- HesapMakinasi.py
+++ HesapMakinasi.py
@@ -2,7 +2,6 @@ sayi1 = int(input("Birinci sayiyi giriniz: "))
 sayi2 = int(input("İkinci sayiyi giriniz: "))
 sayi3 = int(input("Ucuncu sayiyi giriniz: "))
 sayi4 = int(input("Dorduncu sayiyi giriniz: "))
-sayi5 = int(input("sayi1 * sayi3 + sayi4 - sayi2: "))
 
 
 toplam = sayi1 + sayi2
@@ -27,6 +26,3 @@ print("")
 print("Bolme Sonuç:" ,bolme)
 
 
-sayi5 = sayi1 * sayi3 + sayi4 - sayi2
-print("")
-print("sayi5 sonuç:" , sayi5)
