b8238ba601897f7fe469f4cac94d81e32850a628
Bora Saglam <posarobit@gmail.com>
2025-06-04 17:51:50 +0300

    Series.py ekledim

---
+++ Films-Series-Animes.py/Series.py
@@ -0,0 +1,12 @@
+mesaj1 = input("Series Genres")
+
+mesaj2 = input("1. Historical films")
+
+mesaj = input("The Last Kingdom")
+mesaj = input("Outlander")
+mesaj = input("The Crown")
+print = input("Roman Empire")
+print = input("Rise of Empires: Ottoman")
+mesaj = input("Vikings")
+mesaj = input("Marco Polo")
+print = input("Barbarians")
