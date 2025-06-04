84ba69386fd38dfa1e3b367b5e24955b978ee060
Bora Saglam <posarobit@gmail.com>
2025-06-04 17:29:05 +0300

    Films.py değişiklik yaptım

--- Films-Series-Animes.py/Films.py
+++ Films-Series-Animes.py/Films.py
@@ -3,5 +3,13 @@ mesaj = input("Films Genres")
 mesaj = input("1. Science-Fiction films")
 
 mesaj = input("Uzaydan Gelen Firtina")
+mesaj = input("Lucy")
+mesaj = input("Ready Player One")
+print = input("Gelecege Donus")
+print = input("Adam Project")
+mesaj = input("Ask ve Canavarlar")
+mesaj = input("Terminator")
+print = ("Mad Max")
+print = ("Morbius")
 
 
