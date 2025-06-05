e58d4ed3e8b525af6430c80cbd79fa64c0eb4196
Bora Saglam <posarobit@gmail.com>
2025-06-05 10:52:36 +0300

    SiteProgarmi2.0.py ekledim.

---
+++ SiteProgrami2.0.py
@@ -0,0 +1,8 @@
+mesaj = input("Gezi Rotaları")
+mesaj = input("Rota 1")
+
+print = "Tourist spots", "Opining Hours, ve Entry Fees"  
+
+print = "1. Beyaz Kule" 
+print = "Opining Hours: 10.00-18.30"
+print = "Entry Fee = 8€"
