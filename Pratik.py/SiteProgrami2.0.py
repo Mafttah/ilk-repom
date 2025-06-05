212362603b570c4e3aea87109a9227d0c98e24a0
Bora Saglam <posarobit@gmail.com>
2025-06-05 12:08:18 +0300

    SiteProgrami2.0.py değişiklik yaptım.

--- Pratik.py/SiteProgrami2.0.py
+++ Pratik.py/SiteProgrami2.0.py
@@ -1,4 +1,4 @@
-print = input("Gezi Rotaları")
+print = input("Gezi Rotalari")
 print = input("Rota 1")
 
 print = input("Thessaloniki")  
@@ -6,3 +6,31 @@ print = input("Thessaloniki")
 print = input("Tourustic place = 1. Beyaz Kule") 
 print = input("Opining Hours: 10.00-18.30")
 print = input("Entry Fee = 8€")
+
+print = input("Tourustic place = 2. Selanik Arkeoloji Muzesi")
+print = input("Opining Hours: 09.00-18.30")
+print = input("Entry Fee = 10€")
+
+print = input("Tourustic place = 3. Selanik Fotoğrafcilik Muzesi")
+print = input("Opining Hours: Pazartesi hariç 11.00-18.30")
+print = input("Entry Fee = 5€")
+
+print = input("Tourustic place = 4. Bizans Kultur Müzesi")
+print = input("Opining Hours: 09.00-18.30")
+print = input("Entry Fee = 10€")
+
+print = input("Tourustic place = 5. Selanik Yahudi Müzesi")
+print = input("Opining Hours: Pazartesi ve Cumartesi hariç 09.00-13.30")
+print = input("Entry Fee = 8€")
+
+print = input("Tourustic place = 6. Selanik Savaş Müzesi")
+print = input("Opining Hours: 09.00-18.00")
+print = input("Entry Fee = 5€") 
+
+print = input("Tourustic place = 7. Park of National Selanik")
+print = input("Opining Hours: 08.00-14.30")
+print = input("Entry Fee = 30€")
+
+print = input("Tourustic place = 8. Selanik Atatürk Evi Müzesi")
+print = input("Opining Hours: Pazartesi hariç 10.00-17.00")
+print = input("Entry Fee = Free")
