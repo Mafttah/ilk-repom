f23c11d4ba79850a3f895dbcaa0a8333168c05c2
Bora Saglam <posarobit@gmail.com>
2025-06-09 18:06:55 +0300

    Ingilizce.py değişiklik yaptım

--- Pratik.py/Ingilizce.py
+++ Pratik.py/Ingilizce.py
@@ -1,4 +1,4 @@
-import googletrans
 from googletrans import Translator
-translation = translator = "Hello","Hi","Good morning","Good Afternoon","Good Evening" ("dest=fr")
+translator=Translator()
+translation = translator.translate("Hello","Hi","Good morning","Good Afternoon","Good Evening", dest= "fr")
 print("translation")
