6c3d165b78f3dcbfcfdee24cd3a36916fc3c77f7
Bora Saglam <posarobit@gmail.com>
2025-06-09 19:54:37 +0300

    Ingilizce.py değişiklik yaptım.

--- Pratik.py/Ingilizce.py
+++ Pratik.py/Ingilizce.py
@@ -1,7 +1,4 @@
 from googletrans import Translator
 
-trans = Translator()
-t = trans.translate('Hello', src= 'en', dest='fr')
-print(f'Source: {t.src}')
-print(f'Destination: {t.dest}')
-print(f'{t.origin} -> {t.text}')
+translation= "Hello", src =en, dest='fr'
+print(translation)
