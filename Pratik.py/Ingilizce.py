ed9a356b8d27a8692ab0a51854974bb4d5003045
Bora Saglam <posarobit@gmail.com>
2025-06-09 18:48:15 +0300

    Ingilizce.py değişiklik yaptım.

--- Pratik.py/Ingilizce.py
+++ Pratik.py/Ingilizce.py
@@ -2,4 +2,4 @@ from googletrans import Translator
 mesaj = "translator" 
 mesaj = "dest"
 "translator" == "Hello","Good morning","Good Afternoon","Good Evening", "dest" == 'fr'
-print("translator")
+print("mesaj")
